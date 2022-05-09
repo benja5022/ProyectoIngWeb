29b012710071d4710d8f7858cc2c21683c4b2579
benja5022 <benjaui5022@gmail.com>
2022-05-08 20:13:29 -0400

    Se incorporó Bootstrap, nuevas interfaces, etc...

---
+++ js/scripdonar.js
@@ -0,0 +1,11 @@
+function verbancos(){
+    $("#mostrar").fadeIn();
+}
+
+function ocultarbancos(){
+    $("#mostrar").fadeOut();
+    $("#rndm").fadeIn();
+    setTimeout(2000000);
+    $("#rndm").fadeOut();
+
+}
